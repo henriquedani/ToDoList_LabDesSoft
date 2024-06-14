9e6ed5c537264d4cc98c2d5e13b7b8e64f13376d
Henrique Dani <henriquedani172004@gmail.com>
2024-06-14 15:54:11 -0300

    Add roteiro 4 aula 1 e 2

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+export function App(props) {
+  return (
+    <div className='App'>
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TodoWrapper.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { TodoForm } from './TodoForm';
+import { TodoList } from './TodoList';
+import { v4 as uuidv4 } from "uuid";
+
+export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([
+        { id: 1, description: 'Tarefa exemplo', completed: false }
+    ]);
+    
+    const addTodo = (todo) => {
+        setTodos([
+            ...todos,
+            { id: uuidv4(), task : description , completed: false },
+        ]);
+    }
+    return (
+        <div className='TodoWrapper'>
+            <h1>Lista de Tarefas</h1>
+            <TodoForm addTodo={addTodo} />
+            {todos.map((item) =>
+                <TodoList
+                    key={item.id}
+                    task={item}
+                />
+            )
+            }
+        </div>
+    );
+}
